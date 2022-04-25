b26ae82dfc42dc480398ef75b378d0bd3e3db8d2
vanessakhoo23 <vane238life@gmail.com>
2022-04-25 18:41:13 +0800

    TalkWithToadAction Class Modified (REQ 6)

--- src/game/actions/TalkWithToadAction.java
+++ src/game/actions/TalkWithToadAction.java
@@ -3,6 +3,7 @@ package game.actions;
 import edu.monash.fit2099.engine.actions.Action;
 import edu.monash.fit2099.engine.actors.Actor;
 import edu.monash.fit2099.engine.displays.Display;
+import edu.monash.fit2099.engine.items.Item;
 import edu.monash.fit2099.engine.positions.GameMap;
 import game.Utils;
 import game.actors.Player;
@@ -11,6 +12,7 @@ import game.enums.Status;
 import game.weapons.Wrench;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class TalkWithToadAction extends Action {
 
@@ -18,36 +20,37 @@ public class TalkWithToadAction extends Action {
     @Override
     public String execute(Actor actor, GameMap map) {
         // check if player has a wrench
-        boolean hasWrench = actor.getWeapon() instanceof Wrench;
+        boolean hasWrench = false;
+        List<Item> inventory = actor.getInventory();
+        for (Item item : inventory) {
+            if (item instanceof Wrench) {
+                hasWrench = true;
+                break;
+            }
+        }
         // check if player has powerstar effect
         boolean hasPowerStarEffect = actor.hasCapability(Status.INVINCIBLE);
         ArrayList<Integer> indexes = new ArrayList<Integer>();
         String sentence;
         if (hasWrench && !hasPowerStarEffect) {
             indexes.add(1); indexes.add(2); indexes.add(3);
-            sentence = Toad.getSentence(Utils.getRandomFrom(indexes));
         }
         else if (hasWrench && hasPowerStarEffect) {
             indexes.add(2); indexes.add(3);
-            sentence = Toad.getSentence(Utils.getRandomFrom(indexes));
         }
         else if (!hasWrench && hasPowerStarEffect) {
-            indexes.add(2); indexes.add(3);
-            sentence = Toad.getSentence(Utils.getRandomFrom(indexes));
+            indexes.add(0); indexes.add(2); indexes.add(3);
         }
         else {
             indexes.add(0); indexes.add(1); indexes.add(2); indexes.add(3);
-            sentence = Toad.getSentence(Utils.getRandomFrom(indexes));
         }
-
-        Display display = new Display();
-        display.println(sentence);
-        return "You have conversed with Toad!";
+        sentence = Toad.getSentence(Utils.getRandomFrom(indexes));
+        return sentence;
     }
 
     @Override
     public String menuDescription(Actor actor) {
-        return actor + " talks with Toad.";
+        return actor + " talks with Toad";
     }
 
     @Override
