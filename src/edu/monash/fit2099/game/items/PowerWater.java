f44e05bec7e9036a1f9a42f71e28e0a46f1f6fd2
BuildTools <unconfigured@null.spigotmc.org>
2022-05-22 23:50:08 +0800

    Completion of JavaDoc for Water, HealthWater, PowerWater, Bottle

--- src/edu/monash/fit2099/game/items/PowerWater.java
+++ src/edu/monash/fit2099/game/items/PowerWater.java
@@ -3,6 +3,14 @@ package edu.monash.fit2099.game.items;
 import edu.monash.fit2099.engine.actors.Actor;
 import edu.monash.fit2099.game.interfaces.Drinker;
 
+
+/**
+ * Class representing PowerWater, a specific type of Water object that increases the Actor's damage.
+ *
+ * @author Chong Jin Yao
+ * @version 3.0.0
+ * @see edu.monash.fit2099.game.items
+ */
 public class PowerWater extends Water {
 
     /***
@@ -12,6 +20,11 @@ public class PowerWater extends Water {
         super("Power Water", 'p');
     }
 
+
+    /**
+     * Method responsible for performing what happens when the Actor consumes the item/object.
+     * @param actor Actor consuming the item
+     */
     @Override
     public void consumedBy(Actor actor) {
         Drinker actorDrink = (Drinker) actor;
@@ -20,8 +33,14 @@ public class PowerWater extends Water {
 
     }
 
+
+    /**
+     * Implemented method from the ConsumableItem interface that returns True or False based on whether the item can disappear over time.
+     * @return True if can disappear, False if can't
+     */
     @Override
     public boolean canFade() {
         return false;
     }
+
 }
--- src/edu/monash/fit2099/game/items/Water.java
+++ src/edu/monash/fit2099/game/items/Water.java
@@ -3,6 +3,14 @@ package edu.monash.fit2099.game.items;
 import edu.monash.fit2099.engine.items.Item;
 import edu.monash.fit2099.game.interfaces.ConsumableItem;
 
+
+/** Class representing the Water Object in the edu.monash.fit2099.game
+ * Provides the most basic functionality of Water object, subclasses to provide further implement specific functionality
+ *
+ * @author Chong Jin Yao
+ * @version 3.0.0
+ * @see edu.monash.fit2099.game.items
+ */
 public abstract class Water extends Item implements ConsumableItem {
 
     /***
@@ -11,4 +19,5 @@ public abstract class Water extends Item implements ConsumableItem {
     public Water(String name, char displayChar) {
         super(name, displayChar, false);
     }
+
 }
