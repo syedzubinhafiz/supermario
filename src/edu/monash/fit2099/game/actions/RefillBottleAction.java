00b96c5239bb167a9024629fe50d74b9c20c4046
BuildTools <unconfigured@null.spigotmc.org>
2022-05-22 19:26:07 +0800

    Completion of JavaDoc for all classes in Actions Package

--- src/edu/monash/fit2099/game/actions/RefillBottleAction.java
+++ src/edu/monash/fit2099/game/actions/RefillBottleAction.java
@@ -7,15 +7,50 @@ import edu.monash.fit2099.game.grounds.MagicalFountain;
 import edu.monash.fit2099.game.items.Bottle;
 import edu.monash.fit2099.game.items.Water;
 
+
+/**
+ * Special action class tasked with refilling the Players bottle with Water objects from MagicalFountains
+ *
+ * @author Chong Jin Yao
+ * @version 3.0.0
+ * @see edu.monash.fit2099.engine.actions
+ */
 public class RefillBottleAction extends Action {
 
+
+    /**
+     * Attribute
+     * Water object to fill the bottle with.
+     */
     private Water water;
+
+
+    /**
+     * Attribute
+     * Fountain instance that provides the water.
+     */
     private MagicalFountain fountain;
 
+
+    /**
+     * Constructor
+     * @param water Type of water to fill the bottle with.
+     * @param fountain The fountain from which water is to be taken from.
+     */
     public RefillBottleAction(Water water, MagicalFountain fountain){
         this.water = water;
         this.fountain = fountain;
     }
+
+
+    /**
+     * Executes the RefillBottleAction by getting the instance of the singleton Bottle class from the players inventory and
+     * using its in-built methods to add the water.
+     * Removes Water object from the fountain after doing so.
+     * @param actor The actor performing the action.
+     * @param map The map the actor is on.
+     * @return a String to let the player know they have refilled their bottle successfully
+     */
     @Override
     public String execute(Actor actor, GameMap map) {
         // refills the bottle of player
@@ -24,6 +59,12 @@ public class RefillBottleAction extends Action {
         return "Mario refills bottle from " + fountain.getName() + " (" + fountain.getCapacity()+ "/"+fountain.getMaxCapacity()+")";
     }
 
+
+    /**
+     * Returns a descriptive statement for RefillBottleAction
+     * @param actor The actor performing the action.
+     * @return a String describing the action to carry out for menu use.
+     */
     @Override
     public String menuDescription(Actor actor) {
         return "Refill " + actor + "\'s bottle.";
--- src/edu/monash/fit2099/game/actions/ResetGameAction.java
+++ src/edu/monash/fit2099/game/actions/ResetGameAction.java
@@ -5,6 +5,7 @@ import edu.monash.fit2099.engine.actors.Actor;
 import edu.monash.fit2099.engine.positions.GameMap;
 import edu.monash.fit2099.game.managers.ResetManager;
 
+
 /**
  * Special ResetGameAction that allows an actor to reset the edu.monash.fit2099.game.
  *
@@ -14,12 +15,13 @@ import edu.monash.fit2099.game.managers.ResetManager;
  */
 public class ResetGameAction extends Action {
 
+
     /**
      * Attribute to store the singleton ResetManager instance.
      */
     private static final ResetManager manager = ResetManager.getInstance();
 
-    @Override
+
     /**
      * Executes the resetting of the edu.monash.fit2099.game.
      * @param actor The actor performing the action.
@@ -27,27 +29,31 @@ public class ResetGameAction extends Action {
      * @return a String to show the message output after execution.
      * @see Action#execute(Actor actor, GameMap map)
      */
+    @Override
     public String execute(Actor actor, GameMap map) {
         manager.run(map);
         return "You have reset game";
     }
 
-    @Override
+
     /**
      * Returns a descriptive statement for the ResetGameAction
      * @param actor The actor performing the certain action
      * @return a String describing actor resetting the edu.monash.fit2099.game
      */
+    @Override
     public String menuDescription(Actor actor) {
         return "Reset the game.";
     }
 
-    @Override
+
     /**
      * Returns the specific hotkey for this action.
      * @return String representing hotkey
      */
+    @Override
     public String hotkey() {
         return "r";
     }
+
 }
